f6de36a99b38bc60e209b5429be64c149f775e7d
Florent <florent.nolot@univ-reims.fr>
2020-12-06 22:27:30 +0100

    GNU GPLv3 applied

--- default/create-1net.sh
+++ default/create-1net.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#    Copyright (C) 2020 Florent NOLOT
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###########################################################################
+
 if [ $# != 9 ]; then 
 	echo "Usage: $0 filename admin_ip admin_ip_mask data_ip data_ip_mask vlan_number data_bridge delay loss"
 else
